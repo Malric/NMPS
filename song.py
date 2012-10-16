6723f7bc28b7c87cf742fbed92b4613116d07781
Malric <malricdal@gmail.com>
2012-10-16 14:49:11 +0300

    Artist, Song, Uri class created

---
+++ song.py
@@ -0,0 +1,15 @@
+####
+#
+# Artist, Song, Url class
+#
+####
+
+class song:
+
+    def __init__(self, artist, song, uri):
+        self.artist = artist
+        self.song   = song
+        self.URI    = uri
+
+        
+
