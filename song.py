05e80f0d4c7e9e05f888d1cc0c8e5fad1c626b40
Malric <malricdal@gmail.com>
2012-10-16 15:08:31 +0300

    Song class updated

--- song.py
+++ song.py
@@ -1,15 +1,21 @@
 ####
 #
-# Artist, Song, Url class
+# Artist, Song, Url, path classes
 #
 ####
 
-class song:
+class clientSong:
 
     def __init__(self, artist, song, uri):
         self.artist = artist
         self.song   = song
         self.URI    = uri
 
-        
+class serverSong:
+
+    def __init__(self, artist, song, path):
+        self.artist = artist
+        self.song = song
+        self.path = path
+
 
