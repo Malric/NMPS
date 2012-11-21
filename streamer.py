6e71584cc37c0e7a424f76b12d381668a9026980
pokhrel <saurabhpokhrel@gmail.com>
2012-11-21 02:35:05 +0200

    changes in stremer,ltunez-server need to be changed for IPC

--- streamer.py
+++ streamer.py
@@ -2,11 +2,13 @@
 # RTP/RTCP streamer
 ###
 
+import random
 import select
 import socket
 import sys
 import time
 import RTP
+import os
        
 def bind(PORT):
     """ Create UDP socket and bind given port with it. """ 
@@ -30,32 +32,60 @@ def bind(PORT):
         break
     return s
  
+# One streamer per song for all clients
 def main():
-    """ Streamer """
-    fd = int(sys.argv[1])
-    u_sock = socket.fromfd(fd,socket.AF_UNIX, socket.SOCK_STREAM) #Create socket object
-    rtp_sock = bind(9000)
-    if rtp_sock is None:
+    """ Control section for streamer. """
+    # Create unix socket for IPC
+    path = 'Sockets/'+argv[1]
+    if os.path.exists(path): #Caution
         sys.exit(1)
-    rtcp_sock = bind(9001)
-    if rtcp_sock is None:
-        sys.exit(1)
+    unix_socket = socket.socket(socket.AF_UNIX, socket.SOCK_DGRAM)
+    unix_socket.bind(path)
+    # Create rtp and rtcp socket
+    while True:
+        while True:
+            #port = random.randint(10000,65000)
+            port = 9000
+            rtp_socket = bind(port)
+            if rtp_socket is None:
+                continue
+            else:
+                break
+        rtcp_socket = bind(port + 1)
+        if rtcp_socket is None:
+            rtp_socket.close()
+        else:
+            break
     inputs = []
-    inputs.append(u_sock)   # adding unix socket
-    inputs.append(rtcp_sock) # adding rtcp socket
+    inputs.append(unix_socket) # Add unix socket
+    inputs.append(rtcp_socket) # Add rtcp socket
+    # List of client
+    clients = []
+    rtp_header = []
+    # Stream status
+    STREAM = False # Must be present for each client,only one for now,test purpose
     while True:     
         try:
-            inputready,outputready,exceptready = select.select(inputs,[],[])
+            inputready,outputready,exceptready = select.select(inputs,[],[],0)
         except select.error as msg:
             print 'Streamer: '+str(msg)
         for option in inputready:
-            if option is u_sock:
-                data = u_sock.recv(1024)
-                print data
+            if option is unix_socket:
+                data = unix_socket.recv(1024)
+                # IPC message format: Command,parameter,paremeter,...
+                args = data.split(',')
+                if args[0] is 'Setup':
+                    rtp = RTP.RTPMessage(random.randint(10000,60000))
+                    clients.append([int(args[1]),int(args[2]),rtp]) # Append all active clients to list,Find idea to remove
+                elif args[0] is 'Play':
+                    STREAM = True                
             if option is rtcp_sock:
                 data = rtcp_sock.recv(1024)
                 print data
-
+        for client in clients:
+            rtp.sendto(client[2].createMessage(1,2,4),('::1',client[0]))
+        time.sleep(1) # For now            
+        
 if __name__ == "__main__":
     main()
 
