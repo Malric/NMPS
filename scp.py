903a70b7f0887f0c917d878b4d6468f252f8bf94
Mikko Västi <malricdal@gmail.com>
2012-11-26 22:57:03 +0200

    StreamerControlProtocol implementation
    Here is quick and dirty implementation of StreamerControlProtocol, it
    supports following commands:
    
    SETUP, PLAY, PAUSE, TEARDOWN.

---
+++ scp.py
@@ -0,0 +1,91 @@
+####
+# StreamerControlProtocol
+####
+
+import re
+import urllib
+import urlparse
+import logging
+
+#RTSP Commands (Supported)
+commands = ["SETUP", "TEARDOWN", "PLAY", "PAUSE"]
+terminator "\r\n\r\n"
+
+class SCPMessage:
+
+    def __init__(self):
+        self.command 	 	= ""
+        self.protocol 		= "SCP/1.0"
+        self.clientIp		= ""
+        self.clientRtpPort	= ""
+        self.clientRtcpPort = ""
+        self.message = ""
+
+        #REGEX
+        self.ipRegex = re.compile(r'ip: (.*)$', re.IGNORECASE)
+        self.rtpRegex = re.compile(r'rtp: (.*)$', re.IGNORECASE)
+        self.rtcpRegex = re.compile(r'rtcp: (.*)$', re.IGNORECASE)
+
+    def createPlay(self, ip):
+    	self.command = "PLAY"
+    	self.clientIp = ip
+    	self.message = self.command + " "+self.protocol+"\r\n"
+    	self.message += "ip: "+self.clientIp+terminator
+    	return self.message
+
+    def createPause(self, ip):
+    	self.command = "PAUSE"
+    	self.clientIp = ip
+    	self.message = self.command + " "+self.protocol+"\r\n"
+    	self.message += "ip: "+self.clientIp+terminator
+    	return self.message
+
+    def createTeardown(self, ip):
+    	self.command = "TEARDOWN"
+    	self.clientIp = ip
+    	self.message = self.command + " "+self.protocol+"\r\n"
+    	self.message += "ip: "+self.clientIp+terminator
+    	return self.message
+
+    def createSetup(self, ip, rtpPort, rtcpPort):
+    	self.command = "SETUP"
+    	self.clientIp = ip
+    	self.clientRtpPort = rtpPort
+    	self.clientRtcpPort = rtcpPort
+    	self.message = self.command + " "+self.protocol+"\r\n"
+    	self.message += "ip: "+self.clientIp+"\r\n"
+    	self.message += "rtp: "+self.clientRtpPort+"\r\n"
+    	self.message += "rtcp: "+self.clientRtcpPort+terminator
+    	return self.message
+
+  	def parse(self,message):
+  		self.message = message
+
+  		lines = self.message.split('\r\n')
+                   
+        #Line 1: Command, Protocol
+        try:
+            (command, protocol) = lines[0].split()
+        except ValueError:
+            return False
+
+       if protocol != self.protocol:
+            return False
+
+        if command not in commands:
+            return False
+
+        self.command = command
+
+        for line in lines:
+        	hits = self.ipRegex.search(line)
+            if hits is not None:
+                self.clientIp = hits.group(1)
+            hits = self.rtpRegex.search(line)
+            if hits is not None:
+                self.clientRtpPort = hits.group(1)
+            hits = self.rtcpRegex.search(line)
+            if hits is not None:
+                self.clientRtcpPort = hits.group(1)
+               
+
