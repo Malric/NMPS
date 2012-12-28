e9ab9a9cd0ba946b2000826e95a9677ce2f4768f
Malric <malricdal@gmail.com>
2012-12-28 18:44:30 +0200

    Added tag to From field
    Signed-off-by: Malric <malricdal@gmail.com>

--- SDP_sip.py
+++ SDP_sip.py
@@ -6,22 +6,23 @@
 
 import socket
 
+terminator="\r\n"
 
 class SDPMessage:
 
-    def __init__(self, session, subject):
+    def __init__(self, session, subject, ip):
         self.v = "0"
-        self.o = "test " + session + " 654321 IN IP4 " + socket.gethostbyname(socket.gethostname())
+        self.o = "test " + session + " 654321 IN IP4 " + ip
         self.s = subject
-        self.c = "IN IP4 " + socket.gethostbyname(socket.gethostname())
+        self.c = "IN IP4 " + ip
         self.t = "0 0"
         self.m = "audio 8078 RTP/AVP 0 101"
         self.a = "rtpmap:0 PCMU/8000/1"
-        self.SDPMsg = "v=" + self.v + "\r\n"
-        self.SDPMsg += "o=" + self.o + "\r\n"
-        self.SDPMsg += "s=" + self.s + "\r\n"
-        self.SDPMsg += "c=" + self.c + "\r\n"
-        self.SDPMsg += "t=" + self.t + "\r\n"
-        self.SDPMsg += "m=" + self.m + "\r\n"
-        self.SDPMsg += "a=" + self.a
+        self.SDPMsg = "v=" + self.v + terminator
+        self.SDPMsg += "o=" + self.o + terminator
+        self.SDPMsg += "s=" + self.s + terminator
+        self.SDPMsg += "c=" + self.c + terminator
+        self.SDPMsg += "t=" + self.t + terminator
+        self.SDPMsg += "m=" + self.m + terminator
+        self.SDPMsg += "a=" + self.a #+ terminator
         
