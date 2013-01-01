44f2521a9ba05ba7da56ca69f5757cdbd994354b
Malric <malricdal@gmail.com>
2013-01-01 22:42:57 +0200

    RTSP type casting...
    Another try… this time should work ;)
    
    Signed-off-by: Malric <malricdal@gmail.com>

--- sdp.py
+++ sdp.py
@@ -12,15 +12,14 @@ class SDPMessage:
     def __init__(self, subject, session):
         self.v = "0"
         self.s = subject
-        self.o = "Ltunez "+session+" "+NTP.timestamp()+" IN IP4 "\
+        self.o = "Ltunez "+str(session)+" "+NTP.timestamp()+" IN IP4 "\
 				 +socket.gethostbyname(socket.gethostname())
         self.m = "audio 0 RTP/AVP 0" #u-law PCM! <-- Fix me for A-law # removed /2
         self.a = "sendonly"
-       
 	
     def setPort(self, port):
         self.m = "audio "+port+"/2 RTP/AVP 0"
-        
+
     def getMessage(self):
         #print self.sdpMsg
         self.sdpMsg = "v="+self.v+"\r\n"
@@ -29,4 +28,3 @@ class SDPMessage:
         self.sdpMsg +="m="+self.m+"\r\n"
         self.sdpMsg +="a="+self.a
         return self.sdpMsg
-
