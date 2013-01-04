403e8f5499636f6f453e8e6cdaf6770578bbd9f8
Lauri <lauri.koskimies@aalto.fi>
2013-01-05 01:35:48 +0200

    local ip related updates

--- helpers.py
+++ helpers.py
@@ -30,28 +30,28 @@ def bind(PORT):
 # Connects to google dns and gets public ethernet interface ip address of the machine from socket.
 ##
 def tcpLocalIp(): 
-	s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-	s.connect(('8.8.8.8', 80))
-	ret = s.getsockname()[0]
-	s.close()
-	return ret
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s.connect(('8.8.8.8', 80))
+    ret = s.getsockname()[0]
+    s.close()
+    return ret
 
 ##
 # Creates and binds udp socket. Creates another udp socket and sends to binded socket message. Address information is found in the udp recvfrom function
 ##
 def udpLocalIp():
-	server_socket = bind(12345)
-	sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-	sock.sendto("Ping!",(socket.gethostname(),12345))
-	data, address = server_socket.recvfrom(1024)
-	ret = address[0]
-	sock.close()
-	server_socket.close()
-	return ret
+    server_socket = bind(12345)
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    sock.sendto("Ping!",(socket.gethostname(),12345))
+    data, address = server_socket.recvfrom(1024)
+    ret = address[0]
+    sock.close()
+    server_socket.close()
+    return ret
 
 ##
 # Gets public ethernet interface ip address of the machine using dns
 ##
 def sockLocalIp():
-	ret = socket.gethostbyname(socket.getfqdn())
-	return ret
+    ret = socket.gethostbyname(socket.getfqdn())
+    return ret
